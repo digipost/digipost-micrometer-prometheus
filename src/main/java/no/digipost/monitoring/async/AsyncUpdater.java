e4ca706f859d102a75979318632df7f40962d668
Gustav Karlsson <gustav.karlsson@bekk.no>
2020-04-24 09:51:34 +0200

    Reformatting

--- src/main/java/no/digipost/monitoring/async/AsyncUpdater.java
+++ src/main/java/no/digipost/monitoring/async/AsyncUpdater.java
@@ -24,48 +24,48 @@ import java.util.logging.Logger;
 class AsyncUpdater implements Runnable {
 
     private static final Logger LOG = Logger.getLogger(AsyncUpdater.class.getName());
-	private final Runnable updateNewValues;
-	private final Duration updateInterval;
-	private Clock clock;
-	private String updaterName;
-	private Instant lastUpdate;
-	private boolean lastUpdateSuccessful;
+    private final Runnable updateNewValues;
+    private final Duration updateInterval;
+    private Clock clock;
+    private String updaterName;
+    private Instant lastUpdate;
+    private boolean lastUpdateSuccessful;
 
-	AsyncUpdater(Clock clock, String updaterName, Runnable updateNewValues, Duration updateInterval) {
-		this.clock = clock;
-		this.updaterName = updaterName;
-		this.updateNewValues = updateNewValues;
-		this.updateInterval = updateInterval;
-		lastUpdate = clock.instant();
-		lastUpdateSuccessful = true;
-	}
+    AsyncUpdater(Clock clock, String updaterName, Runnable updateNewValues, Duration updateInterval) {
+        this.clock = clock;
+        this.updaterName = updaterName;
+        this.updateNewValues = updateNewValues;
+        this.updateInterval = updateInterval;
+        lastUpdate = clock.instant();
+        lastUpdateSuccessful = true;
+    }
 
-	@Override
-	public void run() {
-		try {
-			updateNewValues.run();
-			lastUpdate = clock.instant();
-			lastUpdateSuccessful = true;
-		} catch (Exception e) {
-			LOG.log(Level.WARNING, "Unexpected exception in updater '" + updaterName + "' while updating metrics.", e);
-			lastUpdateSuccessful = false;
-		}
-	}
+    @Override
+    public void run() {
+        try {
+            updateNewValues.run();
+            lastUpdate = clock.instant();
+            lastUpdateSuccessful = true;
+        } catch (Exception e) {
+            LOG.log(Level.WARNING, "Unexpected exception in updater '" + updaterName + "' while updating metrics.", e);
+            lastUpdateSuccessful = false;
+        }
+    }
 
-	boolean isStale() {
-		Duration sinceLastUpdate = Duration.between(lastUpdate, clock.instant());
-		return sinceLastUpdate.toMillis() > 2 * updateInterval.toMillis();
-	}
+    boolean isStale() {
+        Duration sinceLastUpdate = Duration.between(lastUpdate, clock.instant());
+        return sinceLastUpdate.toMillis() > 2 * updateInterval.toMillis();
+    }
 
-	boolean isFailing() {
-		return !lastUpdateSuccessful;
-	}
+    boolean isFailing() {
+        return !lastUpdateSuccessful;
+    }
 
-	public String getName() {
-		return updaterName;
-	}
+    public String getName() {
+        return updaterName;
+    }
 
-	public Instant getLastUpdate() {
-		return lastUpdate;
-	}
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
 }
